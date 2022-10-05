2d5e8caa55ccbd4fa13e130b35cbaad3efe9a73c
Anna Harder <annaqharder@gmail.com>
2022-10-05 16:35:11 -0600

    commit tree f6dbe816f8d481a7ec6a25dd6b8471597ad297b6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+    const messages = []; 
+    for (let count = 0; count < name.length; count++) {
+        const message = (`Thank you, ${name[count]}, for the wonderful ${event} gift!`); 
+        messages.push(message); 
+    }; 
+    return messages; 
+}
+console.log(messages); 
+
+function countDown() {
+    let i = 10; 
+    while (i >= 0 ) { 
+        console.log(i--); 
+     } 
+    }
